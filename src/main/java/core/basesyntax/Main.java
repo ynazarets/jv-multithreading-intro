b97e5dc1cf45ae5314700b8346ef945a22e3dd56
Yehor Nazarets <nazarets1111@gmail.com>
2025-12-29 21:57:38 +0100

    created thread and daemonThread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+        thread.start();
+
+        MyRunnable runnable = new MyRunnable();
+        Thread thread1 = new Thread(runnable);
+        thread1.setDaemon(true);
+        thread1.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        int count = 0;
+        while (count <= 20) {
+            System.out.println(count++);
+        }
+    }
 }
